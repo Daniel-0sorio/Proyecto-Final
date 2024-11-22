2a7d913f62e8b02cdadeb4fb658cc602018f95c3
Daniel-0sorio <dosorio@usm.cl>
2024-11-22 15:09:01 -0300

    Subir proyecto

---
+++ proyecto/myapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+def home(request):
+  return render(request, 'home.html')
+
+def members(request):
+  return render(request, 'all_members.html')
+
+def mis_denuncias(request):
+  return render(request, 'mis_denuncias.html')
+
+def login(request):
+  return render(request, 'login.html')
+
+def signin(request):
+  return render(request, 'signin.html')
